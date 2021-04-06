3083e4c69dd2789c97b4eeae9c70f9932e15439f
EtskaCoding <etskacoding@gmail.com>
2021-04-06 20:30:59 +0300

    Update EmbedExample.java

--- src/main/java/ml/etska/discordbot/tutorial/EmbedExample.java
+++ src/main/java/ml/etska/discordbot/tutorial/EmbedExample.java
@@ -10,24 +10,24 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
  *
  * @author etska
  */
-public class EmbedExample extends ListenerAdapter{
+public class EmbedExample extends ListenerAdapter {
 
     @Override
     public void onMessageReceived(MessageReceivedEvent e) {
         String[] args = e.getMessage().getContentRaw().split(" ");
-        if(args[0].equalsIgnoreCase(Main.prefix + "embed")) { //Change this to your command
+        if (args[0].equalsIgnoreCase(Main.prefix + "embed")) { //Change this to your command
             TextChannel channel = e.getTextChannel();//Get text channel
             EmbedBuilder embed = new EmbedBuilder();//Get embedbuilder
-            
+
             embed.setTitle("This is title!");//This is your title
             embed.setColor(Color.yellow);//This is your embed sidebar color!
-            embed.setDescription("This is description" 
-            + "\nIf you want new line you can add it!"
-            + "\n\n**Or if you want bold text**");//And that is description!
-            
+            embed.setDescription("This is description"
+                    + "\nIf you want new line you can add it!"
+                    + "\n\n**Or if you want bold text**");//And that is description!
+
             channel.sendMessage(embed.build()).queue();//And finally send/build the embed!
         }
-        
+
     }
-    
+
 }
